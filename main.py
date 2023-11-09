bc7d10ff19695ccc7dfcb9efad12221fec9f20d4
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-11-09 23:43:00 +0530

    feat: Animate path finding process

--- main.py
+++ main.py
@@ -1,107 +1,94 @@
+import os
 import random
-from queue import Queue
+import time
 
-START = "\033[92mS\033[0m"
-END = "\033[92mE\033[0m"
-WALL = "\033[91m▓\033[0m"
-OPEN_SPACE = "\033[94m◌\033[0m"
-PATH = "\033[92m◍\033[0m"
-ROW_SEPARATOR = "\033[91m---+\033[0m"
+START = "\033[92m S \033[0m"
+END = "\033[92m E \033[0m"
+WALL = "\033[91m ▓ \033[0m"
+OPEN_SPACE = "\033[94m ◌ \033[0m"
+PATH = "\033[92m ◍ \033[0m"
+ROW_SEPARATOR = "---+"
 COLUMN_SEPARATOR = "|"
+COMMAND = "cls" if os.name == "nt" else "clear"
 
 
 def generate_maze(size, wall_percentage):
-    maze = [
-        [
-            WALL if random.randint(1, 100) <= wall_percentage else OPEN_SPACE
-            for _ in range(size)
-        ]
-        for _ in range(size)
-    ]
-    maze[0][0] = START
-    maze[size - 1][size - 1] = END
+    elements = [WALL, OPEN_SPACE]
+    weights = [wall_percentage / 100, 1 - wall_percentage / 100]
+    maze = [random.choices(elements, weights, k=size) for _ in range(size)]
+    maze[0][0], maze[size - 1][size - 1] = OPEN_SPACE, OPEN_SPACE
     return maze
 
 
 def print_maze(maze):
-    separator = ROW_SEPARATOR
-    for i, row in enumerate(maze):
+    os.system(COMMAND)
+    maze[0][0], maze[len(maze) - 1][len(maze) - 1] = START, END
+
+    print("\n+" + ROW_SEPARATOR * len(maze[0]))
+    for row in maze:
         print(f"{COLUMN_SEPARATOR}{COLUMN_SEPARATOR.join(row)}{COLUMN_SEPARATOR}")
-        if i < len(maze) - 1:
-            print(separator * (len(row) // 2))
+        print("+" + ROW_SEPARATOR * len(row))
+
+    maze[0][0], maze[len(maze) - 1][len(maze) - 1] = OPEN_SPACE, OPEN_SPACE
 
 
-def find_path(maze, start, end):
-    queue = Queue()
-    queue.put(start)
-    parent = {start: None}
-
-    while not queue.empty():
-        current = queue.get()
-        if current == end:
-            path = []
-            while current is not None:
-                path.append(current)
-                current = parent[current]
-            path.reverse()
-            return path
-
-        row, col = current
-        neighbors = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
-        for neighbor in neighbors:
-            n_row, n_col = neighbor
-            if (
-                0 <= n_row < len(maze)
-                and 0 <= n_col < len(maze[0])
-                and maze[n_row][n_col] != WALL
-                and neighbor not in parent
-            ):
-                queue.put(neighbor)
-                parent[neighbor] = current
-
-    return None
-
-
-def print_path(maze, path):
-    for row, col in path[1:-1]:
+def find_path_dfs(maze, current, end):
+    time.sleep(0.1)
+    row, col = current
+    if not (
+        0 <= row < len(maze)
+        and 0 <= col < len(maze[0])
+        and maze[row][col] == OPEN_SPACE
+    ):
+        return False
+    if current == end:
         maze[row][col] = PATH
+        print_maze(maze)
+        return True
+
+    maze[row][col] = PATH
     print_maze(maze)
 
+    neighbors = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+    for neighbor in neighbors:
+        if find_path_dfs(maze, neighbor, end):
+            return True
 
-def ratinmaze():
-    size = int(input("Enter the size of the maze: "))
-    wall_percentage = int(input("Enter the percentage of walls in the maze: "))
+    maze[row][col] = OPEN_SPACE
+    print_maze(maze)
+    return False
+
+
+def solver():
+    # size = int(input("Enter the size of the maze: "))
+    size = 10
+    wall_percentage = 25
+    maze = None
+
+    option = 2
 
     while True:
-        maze = generate_maze(size, wall_percentage)
-        print("Generated Maze:")
-        print_maze(maze)
-
-        start = (0, 0)
-        end = (size - 1, size - 1)
-
-        path = find_path(maze, start, end)
-
-        if path:
-            print("Path Found:")
-            print_path(maze, path)
-        else:
-            print("No path found from start to end.")
-
-        option = int(
-            input("Options: Print Path (1), Generate Another Puzzle (2), Exit (3)\n")
-        )
         if option == 1:
-            print("Path:")
-            print_path(maze, path)
+            if find_path_dfs(maze, (0, 0), (size - 1, size - 1)):
+                print("\nPath Found!")
+            else:
+                print("\nNo Path Found!")
         elif option == 2:
-            continue
+            print("Generated Maze: ")
+            maze = generate_maze(size, wall_percentage)
+            print_maze(maze)
         elif option == 3:
+            print("Exiting...")
             break
         else:
-            print("Invalid option. Exiting.")
-            break
+            print("Invalid option!")
+
+        print("\nOptions:")
+        print("1. Find Path")
+        print("2. Generate Another Maze")
+        print("3. Exit")
+        option = int(input("# [1/2/3]: "))
 
 
 if __name__ == "__main__":
-    ratinmaze()
+    solver()
